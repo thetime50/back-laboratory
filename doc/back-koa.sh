3be099f1ef8c7243810c49a52dbc835495cd03b2
thetime50 <1004763777@qq.com>
2020-09-03 22:17:43 +0800

    back

--- doc/back-koa.sh
+++ doc/back-koa.sh
@@ -6,4 +6,4 @@ docker logs thetime-node
 docker stop thetime-node
 docker rm -f thetime-node
 
-docker-compose up --force-recreate --build -d
+docker-compose up --force-recreate --build -d #镜像不会重新编译问题
