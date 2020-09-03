cf5e08e31b4796d8a2e291a81fdffdb12105303e
thetime50 <1004763777@qq.com>
2020-09-03 22:05:26 +0800

    package.json

--- doc/back-koa.sh
+++ doc/back-koa.sh
@@ -1,3 +1,9 @@
 docker-compose up -d
 docker image ls
 docker exec -it thetime-node sh 
+
+docker logs thetime-node
+docker stop thetime-node
+docker rm -f thetime-node
+
+docker-compose up --force-recreate --build -d
